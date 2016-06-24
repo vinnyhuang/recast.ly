dedf4875264895a9f8d185341a05fbcb8ce49c53
vinnyhuang <vincent.vwh.huang@gmail.com>
2016-06-24 16:03:40 -0700

    Hook together the Video List and Video Player components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,40 @@
-var App = (props) => (
-  <div>
-    <Nav />
-    <div className="col-md-7">
-      <VideoPlayer video={props.videos[0]}/>
-    </div>
-    <div className="col-md-5">
-      <VideoList videos={props.videos}/>
-    </div>
-  </div>
-);
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      allVideos: exampleVideoData,
+      currentVideo: exampleVideoData[0]
+    };
+  }
+
+  // onClickPlayVideo(video) {
+  //   this.setState({
+  //     currentVideo: video
+  //   });
+  // }
+
+  onClickPlayVideo(video) {
+    this.setState({
+      currentVideo: video
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Nav />
+        <div className="col-md-7">
+          <VideoPlayer video={this.state.currentVideo}/>
+        </div>
+        <div className="col-md-5">
+          <VideoList clickCallback={this.onClickPlayVideo.bind(this)} videos={this.state.allVideos}/>
+        </div>
+      </div>
+    );
+  }
+
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,6 +1,6 @@
 var VideoList = (props) => (
   <div className="video-list media">
-    {props.videos.map(video => <VideoListEntry video={video}/>)}
+    {props.videos.map(video => <VideoListEntry clickCallback={props.clickCallback} video={video}/>)}
   </div>
 );
 
